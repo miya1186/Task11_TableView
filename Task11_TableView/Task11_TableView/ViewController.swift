3797ea63b0f847990de6570e2307bfdf36625479
miya1186 <66915986+miya1186@users.noreply.github.com>
2020-08-16 07:24:51 +0900

    first commit

---
+++ Task11_TableView/Task11_TableView/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Task11_TableView
+//
+//  Created by miyazawaryohei on 2020/08/16.
+//  Copyright © 2020 miya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    @IBAction func exit(segue:UIStoryboardSegue){}
+    
+    
+}
+
