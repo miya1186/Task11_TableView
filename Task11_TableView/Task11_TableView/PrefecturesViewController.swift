6b0a47ea9652a4c7aefce57aec4766d8f8af0c12
miya1186 <66915986+miya1186@users.noreply.github.com>
2020-08-18 00:29:07 +0900

    fix privateで宣言、return文の省略

--- Task11_TableView/Task11_TableView/PrefecturesViewController.swift
+++ Task11_TableView/Task11_TableView/PrefecturesViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 
 class PrefecturesViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     
-    let prefectures = ["北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県","茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県","新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県","静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県","鳥取県","島根県","岡山県","広島県","山口県","徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県","沖縄県"]
+    //privateで宣言
+   private let prefectures = ["北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県","茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県","新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県","静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県","鳥取県","島根県","岡山県","広島県","山口県","徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県","沖縄県"]
     
     var selectedPre = String()
-    @IBOutlet var tableView: UITableView!
+    
+    //privateで宣言
+    @IBOutlet private var tableView: UITableView!
     
     
     override func viewDidLoad() {
@@ -29,7 +32,8 @@ class PrefecturesViewController: UIViewController,UITableViewDelegate,UITableVie
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return prefectures.count
+        //コードが１行だけなのでreturn文省略
+        prefectures.count
     }
     
     
